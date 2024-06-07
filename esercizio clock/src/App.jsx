793491babcbc1e4eedb3739f58aec56d302fd86d
ktr505 <francescamta3@gmail.com>
2024-06-07 18:40:28 +0200

    fix

--- esercizio clock/src/App.jsx
+++ esercizio clock/src/App.jsx
@@ -1,10 +1,13 @@
 
+import Clock from "./Clock";
 import { Counter } from "./Counter";
 
 export function App() {
   return (
     <div>
-      <Counter initialValue={0} incrementAmount={1} />
+      {/* <Counter initialValue={0} incrementAmount={1} /> */}
+      <h1>My React App</h1>
+      <Clock/>
     </div>
   );
 }
