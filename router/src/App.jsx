1316b300ffd2fdb40b526c1bd040071c26a845d3
ktr505 <francescamta3@gmail.com>
2024-07-29 10:55:56 +0200

    router 4

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import ShowGithubUser from './ShowGithubUser';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Welcome from './welcome';
+import ShowGithubUser from './ShowGithubUser';
 
 const App = () => {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/users/ktr505">GitHub User (ktr505)</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-      <Route path="/" element={<Welcome name="User" />} />
+        <Route path="/" element={<Welcome name="User" />} />
         <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </Router>
